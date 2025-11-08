a1fc3c44722f0159554846a8700a1776834a6376
aozodbek2197 <aozodbek2197@gmail.com>
2025-11-08 14:23:12 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# # 1-misol
+# sonlar = [1, 2, 3, 4, 5]
+# kvadratlar = list(map(lambda x: x ** 2, sonlar))
+# print("1. Kvadratlar:", kvadratlar)
+
+# # 2-misol
+# sonlar = [-2, -1, 0, 1, 2, 3, 4]
+# musbatlar = list(filter(lambda x: x > 0, sonlar))
+# natija = list(map(lambda x: x * 2, musbatlar))
+# print("2. Musbat sonlar ikki barobar:", natija)
+
+# # 3-misol
+# satrlar = ["salom", "python", "dunyo"]
+# katta = list(map(lambda s: s.upper(), satrlar))
+# print("3. Katta harfli satrlar:", katta)
+
+# # 4-misol
+# sonlar = [2, 3, 4, 6, 9, 12, 14]
+# bolinadiganlar = list(filter(lambda x: x % 3 == 0, sonlar))
+# print("4. 3 ga bo‘linadiganlar:", bolinadiganlar)
+
+# # 5-misol
+# ebob = lambda a, b: gcd(a, b)
+# print("5. EBOB:", ebob(24, 36))
